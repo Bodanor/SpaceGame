14a66f4dbe07f476f4d2012ef20d1038004d25b6
bodanor <papadopoulos.chris@icloud.com>
2020-12-15 17:32:30 +0100

    Supprimé quelques fichiers inutiles

--- boucle.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def chaque_iteration(liste):
-
-      for nombre in liste:
-            random_nombre = random.randint(0,4)
-            if random_nombre == nombre:
-                  random_nombre = random.randint(0,4)
--- max.py
+++
@@ -1,6 +0,0 @@
-def collision_planete(PLANETE_EN_LISTE, position_vaisseau):
-
-    for planete in PLANETE_EN_LISTE:
-        #Test collision dans l'ordre BAS,HAUT,DROITE,GAUCHE
-        if position_vaisseau[1] <= position(planete)[1] + TAILLE_PLANETE and position_vaisseau[1]+VAISSEAU_HAUTEUR >= position(planete)[1] and position(planete)[0] <= position_vaisseau[0]+VAISSEAU_LARGEUR and position_vaisseau[0] <= position(planete)[0]+TAILLE_PLANETE:
-            if position_vaisseau[1] <= position(planete)[1] + TAILLE_PLANETE*(7/8) and position_vaisseau[1]+VAISSEAU_HAUTEUR >= position(planete)[1]+ TAILLE_PLANETE*(1/8) and position(planete)[0] + TAILLE_PLANETE*(1/8) <= position_vaisseau[0]+VAISSEAU_LARGEUR and position_vaisseau[0] <= position(planete)[0]+TAILLE_PLANETE*(7/8):
